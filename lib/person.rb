1ce6df50dc4d31e75082aae085b2cfdb94848184
Jerry Velazquez <jvr572@gmail.com>
2019-04-03 16:24:22 -0400

    "done"

--- lib/person.rb
+++ lib/person.rb
@@ -3,18 +3,25 @@ require 'clamp'
 
 class Person
 
-  attr_accessor :bank_account, :happiness, :hygiene
-  attr_reader :name
+  attr_accessor :bank_account
+  attr_reader :name, :happiness, :hygiene
 
-  
   def initialize(name)
     @name = name
     @bank_account = 25
-    @happiness = 8.clamp(0, 10)
-    @hygiene = 8.clamp(0, 10)
+    @happiness = 8
+    @hygiene = 8
   end
-
-
+  def happiness=(num)
+    @happiness = num
+    @happiness = 10 if @happiness > 10
+    @happiness = 0 if @happiness < 0
+  end
+  def hygiene=(num)
+    @hygiene = num
+    @hygiene = 10 if @hygiene > 10
+    @hygiene = 0 if @hygiene < 0
+  end
   def clean?
     if @hygiene > 7
       return true
@@ -42,7 +49,7 @@ class Person
   end
 
   def work_out
-    self.hapiness += 2
+    self.happiness += 2
     self.hygiene -= 3
     return "♪ another one bites the dust ♫"
   end
@@ -50,18 +57,18 @@ class Person
   def call_friend(friend)
     self.happiness += 3
     friend.happiness += 3
-    return "Hi #{friend.name}! It's #{name} How are you?"
+    return "Hi #{friend.name}! It's #{name}. How are you?"
   end
 
   def start_conversation(person, topic)
     case topic
       when "politics"
-        person.hapiness -= 1
-        self.hapiness -= 1
+        person.happiness -= 2
+        self.happiness -= 2
         return "blah blah partisan blah lobbyist"
       when "weather"
-        person.hapiness += 1
-        self.hapiness += 1
+        person.happiness += 1
+        self.happiness += 1
         return "blah blah sun blah rain"
       else
         "blah blah blah blah blah"
