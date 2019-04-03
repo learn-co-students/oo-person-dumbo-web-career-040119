3cc936d307325320f6ee3620f3d65373b676499c
Jerry Velazquez <jvr572@gmail.com>
2019-04-03 12:06:12 -0400

    "created class"

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,16 @@
-# your code goes here
+class Person
+
+  attr_accessor :happiness, :hygiene, :bank_account
+
+def initialize(name)
+  @name = name
+  @bank_account = 25
+  @happiness = 8
+  @hygiene = 8
+  binding.pry
+end
+
+def clean?
+
+end
+end
