fcae65ab74cac15f58c33ca9e864edbc8563dc46
Codey Pinero <codey.pinero@gmail.com>
2019-04-08 19:51:37 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,84 +1,71 @@
 require 'pry'
 
 class Person
-	attr_accessor :happiness_points
-	attr_accessor :hygiene_points
-	attr_reader :name
-	attr_reader :bank_account
-
-	def initialize(name,happiness_points=8,hygiene_points=8, balance = 25)
-		@name = name
-		@happiness_points = happiness_points
-		@hygiene_points = hygiene_points
-		@bank_account = balance
-	end
-
-	#un settable
-	
-
-	def incrementer(curr_points, new_val)
-		@new_points = curr_points + new_val
-		@new_points.clamp(0, 10)
-	end
-
-	def decrementer(curr_points,new_val)
-		@new_points = curr_points - new_val
-		@new_points.clamp(0, 10)
-	end
-
-	def get_paid(increase)
-		@bank_account += increase
-		"all about the benjamins"
-	end
-
-	def withdrawal(decrease)
-		@bank_account -= decrease
-	end
-
-	def clean?(hygiene_points)
-		hygiene_points > 7
-	end
-
-	def happy?(happiness_points)
-		happiness_points > 7
-	end
-
-	def take_bath
-		self.hygiene_points = self.incrementer(self.hygiene_points, 4)
-		"♪ Rub-a-dub just relaxing in the tub ♫"
-	end
-
-	def work_out
-		self.happiness_points = self.incrementer(self.happiness_points, 2)
-		self.hygiene_points = self.decrementer(self.hygiene_points, 3)
-		"♪ another one bites the dust ♫"
-	end
-
-	def call_friend(friend)
-		self.happiness_points = self.incrementer(self.happiness_points, 3)
-		friend.happiness_points = friend.incrementer(friend.happiness_points, 3)
-
-		"Hi #{friend.name}! It's #{self.name}. How are you?"
-	end
-
-	def start_conversation(friend, topic)
-		case topic
-		when "politics"
-			self.happiness_points = self.decrementer(self.happiness_points, 1)
-			friend.happiness_points = friend.decrementer(friend.happiness_points, 1)
-			"blah blah partisan blah lobbyist"
-		when "weather"
-			self.happiness_points = self.incrementer(self.happiness_points, 1)
-			friend.happiness_points = friend.incrementer(friend.happiness_points, 1)
-			"blah blah sun blah rain"
-		else
-			"blah blah blah blah blah"
-		end
-	end
-
+  
+  attr_reader :name
+  attr_accessor :bank_account, :happiness, :hygiene
+  
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+  
+  def happiness
+      @happiness.clamp(0, 10)
+  end
+  
+  def hygiene
+    @hygiene.clamp(0, 10)
+  end
+  
+  def happy?
+    happiness > 7 ? true : false
+  end
+  
+  def clean?
+    hygiene > 7 ? true : false
+  end
+  
+  def get_paid(salary)
+    @bank_account += salary
+    return "all about the benjamins"
+  end
+  
+  def take_bath
+    self.hygiene += 4
+    return '♪ Rub-a-dub just relaxing in the tub ♫'
+  end
+  
+  def work_out
+    self.hygiene -= 3
+    self.happiness += 2
+    return "♪ another one bites the dust ♫"
+  end
+  
+  def call_friend(friend)
+    friend.happiness += 3
+    self.happiness += 3
+    return "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
+  
+  def start_conversation(person, topic)
+    case topic
+    when "politics"
+      person.happiness -= 2
+      self.happiness -= 2
+      return 'blah blah partisan blah lobbyist'
+    when "weather"
+      person.happiness += 1
+      self.happiness += 1
+      return 'blah blah sun blah rain'
+    else
+      return 'blah blah blah blah blah'
+    end
+  end
+    
+  
 end
 
-codey = Person.new("codey")
-brenden = Person.new("Brenden")
-
-binding.pry 
+#binding.pry 
